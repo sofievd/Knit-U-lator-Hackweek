9efaa753dd29e869624af0f010db4ac55f5203d8
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-25 08:50:05 +0200

    added react import

--- frontend/src/routes/category.$type.tsx
+++ frontend/src/routes/category.$type.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import PatternCategory from '../components/PatternCategory'
+import react from 'react'
 
 export const Route = createFileRoute('/category/$type')({
   component: PatternCategory,
