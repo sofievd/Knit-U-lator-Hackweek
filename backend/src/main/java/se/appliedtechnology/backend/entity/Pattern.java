b4bf14315d314ea598e82f0f2d802dacd0adfdc9
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-27 11:36:30 +0200

    add Pattern model to be able to save and get patterns

---
+++ backend/src/main/java/se/appliedtechnology/backend/entity/Pattern.java
@@ -0,0 +1,122 @@
+package se.appliedtechnology.backend.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import se.appliedtechnology.backend.dto.SectionDto;
+import tools.jackson.databind.JsonNode;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "patterns")
+public class Pattern {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String patternType;
+
+    @Column(name = "pattern_variant_id", nullable = false, updatable = false)
+    private int patternVariantId;
+
+    @Column(columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> parameters;
+
+    @Column(nullable = false, columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private List<SectionDto> structure;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    public Pattern() {
+    }
+
+    public Pattern(String name, String patternType, int patternVariantId, Map<String, Object> parameters, List<SectionDto> structure) {
+        this.name = name;
+        this.patternType = patternType;
+        this.patternVariantId = patternVariantId;
+        this.parameters = parameters;
+        this.structure = structure;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatternType() {
+        return patternType;
+    }
+
+    public void setPatternType(String patternType) {
+        this.patternType = patternType;
+    }
+
+    public int getPatternVariantId() {
+        return patternVariantId;
+    }
+
+    public void setPatternVariantId(int patternVariant) {
+        this.patternVariantId = patternVariant;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, Object> parameters) {
+        this.parameters = parameters;
+    }
+
+    public List<SectionDto> getStructure() {
+        return structure;
+    }
+
+    public void setStructure(List<SectionDto> structure) {
+        this.structure = structure;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
