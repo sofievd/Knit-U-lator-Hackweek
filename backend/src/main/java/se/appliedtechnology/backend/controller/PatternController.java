20104bc62fec447e2933a2b958ffc85ae206b8f8
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-27 13:11:14 +0200

    added getById method

--- backend/src/main/java/se/appliedtechnology/backend/controller/PatternController.java
+++ backend/src/main/java/se/appliedtechnology/backend/controller/PatternController.java
@@ -4,41 +4,48 @@ import org.springframework.web.bind.annotation.*;
 import se.appliedtechnology.backend.dto.PatternResponse;
 import se.appliedtechnology.backend.dto.SockPatternRequest;
 import se.appliedtechnology.backend.entity.PatternTemplate;
-import se.appliedtechnology.backend.service.PatternSaveService;
 import se.appliedtechnology.backend.service.PatternService;
+import se.appliedtechnology.backend.service.PatternGeneratorService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/patterns")
 @CrossOrigin(origins = "*")
 public class PatternController {
 
+    private final PatternGeneratorService patternGeneratorService;
     private final PatternService patternService;
-    private final PatternSaveService saveService;
 
-    public PatternController(PatternService patternService, PatternSaveService saveService) {
-        this.patternService = patternService;
-        this.saveService = saveService;
+    public PatternController(PatternGeneratorService patternGeneratorService, PatternService saveService) {
+        this.patternGeneratorService = patternGeneratorService;
+        this.patternService = saveService;
     }
 
     @PostMapping("/generate")
     public PatternResponse generatePattern(@RequestBody SockPatternRequest request) {
 
-       PatternResponse generatedPattern =  patternService.generateSockPattern(request);
+       PatternResponse generatedPattern =  patternGeneratorService.generateSockPattern(request);
         String name = (String) request.name();
 
        PatternResponse finalPattern = new PatternResponse(
                name != null ? name : generatedPattern.name(),
                generatedPattern.parameters(),
                generatedPattern.sections());
-       saveService.savePattern("sock", 1, finalPattern);
+       patternService.savePattern("sock", 1, finalPattern);
         return finalPattern;
     }
 
     @GetMapping
     public List<PatternTemplate> getTemplate() {
-        return patternService.getTemplate();
+        return patternGeneratorService.getTemplate();
+    }
+
+    @GetMapping("/{id}")
+    public PatternResponse getPattern(@PathVariable String id){
+        UUID uuid = UUID.fromString(id);
+       return patternService.getById(uuid);
     }
 
 }
