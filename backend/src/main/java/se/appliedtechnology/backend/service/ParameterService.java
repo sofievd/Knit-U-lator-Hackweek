6f79494f7e2229ec9eb827a11128a62012f987d5
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-27 11:44:14 +0200

    update ParameterService

--- backend/src/main/java/se/appliedtechnology/backend/service/ParameterService.java
+++ backend/src/main/java/se/appliedtechnology/backend/service/ParameterService.java
@@ -8,36 +8,39 @@ import java.util.Map;
 
 @Service
 public class ParameterService {
+     private final int GAUCH_CM = 10;
+     private final int GAUCHE_INCH = 4;
 
     public Map<String, Object> generateSock(SockPatternRequest request) {
-        double footcir = request.footCircumference();
-        double stichGauge = request.gauge();
-        double rowGauge = request.gauge();
-        double footLenght = request.footLength();
+        double footCircumference = request.footCircumference();
+        double stitchGauge = request.stitchGauge();
+        double rowGauge = request.rowGauge();
+        double footLength = request.footLength();
 
-        double stitchesPerCm = stichGauge / 10;
+        double stitchesPerCm = stitchGauge / GAUCH_CM;
+        double rowsPerCm = rowGauge/ GAUCH_CM;
 
-        int finalsStiches = 8;
+        int finalStitches = 8;
         int decreases = 4;
-        int castOn = (int) Math.round(footcir * stitchesPerCm);
+        int castOn = (int) Math.round(footCircumference * stitchesPerCm);
         int needleCount = request.needleCount();
-        int stichesPerNeedle = castOn / needleCount;
-        int heelStiches = castOn / 2;
-        int heelFlapRepeats = heelStiches / 2;
+        int stitchesPerNeedle = castOn / needleCount;
+        int heelStitches = castOn / 2;
+        int heelFlapRepeats = heelStitches / 2;
         int gussetPerSide = heelFlapRepeats + 1;
-        int toeDecreaseRounds = (int) Math.ceil(castOn - finalsStiches) / decreases;
+        int toeDecreaseRounds = (int) Math.ceil(castOn - finalStitches) / decreases;
 
 
         Map<String, Object> params = new HashMap<>();
         params.put("cast_on", castOn);
-        params.put("stitches_per_needle", stichesPerNeedle);
+        params.put("stitches_per_needle", stitchesPerNeedle);
         params.put("needle_count", needleCount);
-        params.put("heel_flap_stitches", heelStiches);
+        params.put("heel_flap_stitches", heelStitches);
         params.put("heel_flap_repats", heelFlapRepeats);
         params.put("gusset_pickup_per_side", gussetPerSide);
-        params.put("foot_lengt_cm", footLenght);
+        params.put("foot_lengt_cm", footLength);
         params.put("toe_repat_pairs", toeDecreaseRounds);
-        params.put("final_stiches", finalsStiches);
+        params.put("final_stiches", finalStitches);
 
         return params;
     }
