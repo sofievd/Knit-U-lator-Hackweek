e1b8d37ea5708a0b746e7ba39a3ccf59b2071675
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-27 13:04:47 +0200

    rename of service

---
+++ backend/src/main/java/se/appliedtechnology/backend/service/PatternGeneratorService.java
@@ -0,0 +1,48 @@
+package se.appliedtechnology.backend.service;
+
+import org.springframework.stereotype.Service;
+import se.appliedtechnology.backend.dto.PatternResponse;
+import se.appliedtechnology.backend.dto.SectionDto;
+import se.appliedtechnology.backend.dto.SockPatternRequest;
+import se.appliedtechnology.backend.entity.PatternTemplate;
+import se.appliedtechnology.backend.repository.PatternTemplateRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PatternGeneratorService {
+
+    private final PatternTemplateRepository templateRepository;
+    private final ParameterService parameterService;
+    private final TemplateRenderer renderer;
+    private final PatternBuilder builder;
+
+    public PatternGeneratorService(PatternTemplateRepository repository, ParameterService parameterService, TemplateRenderer renderer, PatternBuilder builder) {
+        this.templateRepository = repository;
+        this.parameterService = parameterService;
+        this.renderer = renderer;
+        this.builder = builder;
+    }
+
+    public PatternResponse generateSockPattern(SockPatternRequest request) {
+        List<PatternTemplate> templates = templateRepository.findByPatternTypeAndPatternVariantId("sock", 1);
+
+        Map<String, Object> params = parameterService.generateSock(request);
+
+        List<SectionDto> sections = builder.build(templates, params, renderer);
+
+        return new PatternResponse(request.name(), params, sections);
+
+
+    }
+
+    public List<PatternTemplate> getTemplate() {
+        List<PatternTemplate> templates = templateRepository.findByPatternTypeAndPatternVariantId("sock", 1);
+        for (PatternTemplate template : templates) {
+            System.out.println(template);
+        }
+        return templates;
+    }
+}
+
