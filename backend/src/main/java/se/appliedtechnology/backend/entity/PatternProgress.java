27bd3674e7a5edbd1738b2c7a6e9f52e750feabb
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-28 09:19:11 +0200

    added PatternProgress

---
+++ backend/src/main/java/se/appliedtechnology/backend/entity/PatternProgress.java
@@ -0,0 +1,75 @@
+package se.appliedtechnology.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "pattern_progress",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"patternId", "sectionIndex", "stepIndex"}
+        ))
+public class PatternProgress {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private UUID patternId;
+
+    private int sectionIndex;
+    private int stepIndex;
+
+    private boolean completed;
+
+    public PatternProgress(UUID patternId, int sectionIndex, int stepIndex, boolean completed) {
+        this.patternId = patternId;
+        this.sectionIndex = sectionIndex;
+        this.stepIndex = stepIndex;
+        this.completed = completed;
+    }
+
+    public PatternProgress() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getPatternId() {
+        return patternId;
+    }
+
+    public void setPatternId(UUID patternId) {
+        this.patternId = patternId;
+    }
+
+    public int getSectionIndex() {
+        return sectionIndex;
+    }
+
+    public void setSectionIndex(int sectionIndex) {
+        this.sectionIndex = sectionIndex;
+    }
+
+    public int getStepIndex() {
+        return stepIndex;
+    }
+
+    public void setStepIndex(int stepIndex) {
+        this.stepIndex = stepIndex;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
