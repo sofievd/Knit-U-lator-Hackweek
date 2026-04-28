97cb419b235128fb6f9708f373ec83b39edaa7cf
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-04-28 15:31:12 +0200

    added security

--- backend/src/main/java/se/appliedtechnology/backend/security/SecurityConfig.java
+++ backend/src/main/java/se/appliedtechnology/backend/security/SecurityConfig.java
@@ -2,10 +2,16 @@ package se.appliedtechnology.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -13,14 +19,31 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception {
-        return httpSecurity.authorizeHttpRequests(
-                auth -> auth
-                        .requestMatchers("/api/patterns/template").permitAll()
-                        .anyRequest().authenticated())
+        return httpSecurity
                 .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/patterns/public/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(
                         (oauth2) ->
                                 oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
